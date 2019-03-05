a67e2fc66e7e0e5f0406c47f2a2cdd84b5393925
Galina Martiushova (patronovskiy) <patronovskiy@mail.ru>
2019-03-05 18:02:01 +0500

    feat: add function for calculate love triangles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,39 @@
 /**
- * @param preferences - an array of integers. Indices of people, whom they love
+ * @param preferences - an preferencesay of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+if (preferences.length < 3) return 0;
+
+  let counter = 0;
+
+  let i = 0;
+
+  while (i < preferences.length) {
+
+    let first = preferences[i];
+
+    if (!first===0) {
+      i ++;
+      continue;
+    }
+    let second = preferences[first - 1];
+    if (!second===0) {
+      i ++;
+      continue;
+    }
+    let third = preferences[second - 1];
+    if (!third || third === first) {
+      i ++;
+      continue;
+    }
+    if (preferences[third - 1] === first) {
+      counter ++;
+      preferences[i] = 0;
+      preferences[first - 1] = 0;
+      preferences[second - 1] = 0;
+    }
+    i ++;
+  }
+  return counter;
+}
